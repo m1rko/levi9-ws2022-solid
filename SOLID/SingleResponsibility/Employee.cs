836af98f978c75ab428a478b0647c96b8628f785
Mirko Mijajlović <mirko993@gmail.com>
2021-12-08 12:00:23 +0100

    Employee example

--- SOLID/SingleResponsibility/Employee.cs
+++ SOLID/SingleResponsibility/Employee.cs
@@ -1,7 +1,14 @@
-﻿public class Employee {
-    public string Name { get; set; }
+﻿public class Employee
+{
+    private string _name;
 
-    public string GetName() {
-        return Name;
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void PrintTimeSheetReport()
+    {
+        Console.WriteLine("Printing a time sheet report...");
     }
 }
