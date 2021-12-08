34ca85e9860b290641aed1fe3d1e71027be18f42
Mirko Mijajlović <mirko993@gmail.com>
2021-12-08 15:53:08 +0100

    OpenClosed applied

---
+++ SOLID/OpenClosed/Air.cs
@@ -0,0 +1,15 @@
+﻿namespace SOLID.OpenClosed
+{
+    internal class Air : IShipping
+    {
+        public decimal GetCost(Order order)
+        {
+            return Math.Max(20, order.GetTotalWeight() * 3);
+        }
+
+        public DateTime GetDate(Order order)
+        {
+            return DateTime.Now.AddDays(1);
+        }
+    }
+}
