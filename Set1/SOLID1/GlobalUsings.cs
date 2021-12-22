edfb3346d5336c655ce90bc7d81d35c3bd727dfa
Mirko Mijajlović <mirko993@gmail.com>
2021-12-22 10:33:45 +0100

    Open Closed added

--- Set1/SOLID1/GlobalUsings.cs
+++ Set1/SOLID1/GlobalUsings.cs
@@ -4,7 +4,7 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using SOLID1.SingleResponsibility;
-//global using SOLID1.OpenClosed;
+global using SOLID1.OpenClosed;
 //global using SOLID1.LiskovSubstitution;
 //global using SOLID1.InterfaceSegragation;
 //global using SOLID1.DependencyInversion;
--- Set1/SOLID1/Program.cs
+++ Set1/SOLID1/Program.cs
@@ -10,3 +10,15 @@ var email = new Email
 email.InsertPicture(new byte[] { });
 email.Send();
 
+
+
+// OpenClosed
+
+var place = new MapsPlace { Coordinates = new[] { 5.3817077m, 20.3906528m } };
+var category = "restaurant";
+place.SetCategory(category);
+var subcategories = place.GetSubcategories();
+
+Console.WriteLine($"Subcategories for the '{ category }' are:");
+foreach (var subcategory in subcategories)
+    Console.WriteLine(subcategory);
