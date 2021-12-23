7aa94f6f5d002f961af823c4075c302c6ba5df0a
Mirko Mijajlović <mirko993@gmail.com>
2021-12-23 14:46:06 +0100

    Liskov Substitution added

---
+++ Set1/SOLID1/LiskovSubstitution/Event.cs
@@ -0,0 +1,28 @@
+﻿namespace SOLID1.LiskovSubstitution
+{
+    internal class Event
+    {
+        public string Title { get; set; }
+
+        public IEnumerable<string> Atendees { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly StartDate { get; private set; }
+        public DateOnly EndDate { get; private set; }
+        public TimeOnly StartTime { get; private set; }
+        public TimeOnly EndTime { get; private set; }
+
+        public void SetDate(DateTime start, DateTime end)
+        {
+            StartDate = DateOnly.FromDateTime(start);
+            EndDate = DateOnly.FromDateTime(end);
+        }
+
+        public virtual void SetTime(DateTime start, DateTime end)
+        {
+            StartTime = TimeOnly.FromDateTime(start);
+            EndTime = TimeOnly.FromDateTime(end);
+        }
+    }
+}
--- Set1/SOLID1/Program.cs
+++ Set1/SOLID1/Program.cs
@@ -12,7 +12,7 @@ email.Send();
 
 
 
-// OpenClosed
+//// OpenClosed
 
 var place = new MapsPlace { Coordinates = new[] { 5.3817077m, 20.3906528m } };
 var category = "restaurant";
@@ -22,3 +22,25 @@ var subcategories = place.GetSubcategories();
 Console.WriteLine($"Subcategories for the '{ category }' are:");
 foreach (var subcategory in subcategories)
     Console.WriteLine(subcategory);
+
+
+
+// LiskovSubstitution
+
+var firstEvent = new Event { Title = "Breakfast", Atendees = new List<string> { "John Doe <john.doe@example.com>, Will Smith <will.smith@example.com>" }, Description = "The menu is on the link below" };
+var secondEvent = new Event { Title = "Meeting", Atendees = new List<string> { "John Doe <john.doe@example.com>, Will Smith <will.smith@example.com>" }, Description = "Sprint planning" };
+var thirdEvent = new AllDayEvent { Title = "Conference", Atendees = new List<string> { "John Doe <john.doe@example.com>, Will Smith <will.smith@example.com>" }, Description = "Tech Conference 2022" };
+
+var calendar = new Calendar();
+calendar.CreateEvent(firstEvent.Title, firstEvent.Atendees, firstEvent.Description, DateTime.Parse("02/14/2022 08:00 AM", CultureInfo.InvariantCulture), DateTime.Parse("02/14/2022 08:30 AM", CultureInfo.InvariantCulture));
+calendar.CreateEvent(secondEvent.Title, secondEvent.Atendees, secondEvent.Description, DateTime.Parse("02/14/2022 12:15 PM", CultureInfo.InvariantCulture), DateTime.Parse("02/14/2022 01:15 PM", CultureInfo.InvariantCulture));
+calendar.CreateEvent(thirdEvent.Title, thirdEvent.Atendees, thirdEvent.Description, DateTime.Parse("02/15/2022", CultureInfo.InvariantCulture), DateTime.Parse("02/14/2022", CultureInfo.InvariantCulture), true);
+
+calendar.ListAllStartEndTime();
+
+
+
+
+
+// Common
+Console.ReadLine();
