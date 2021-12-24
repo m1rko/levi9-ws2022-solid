bfca37b7d830f4c1159d3dbc3ccaa35b6fc9ecbd
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 10:32:44 +0100

    Set2 - Single Responsibility added

---
+++ Set2/SOLID2/SOLID2/Program.cs
@@ -0,0 +1,11 @@
+﻿// SingleResponsibility
+
+var order = new Order
+{
+    OrderNumber = 1,
+    FiscalBillHeader = "Company Name",
+    Items = new List<string> { "Pizza", "Coffee" },
+    Total = 5
+};
+
+order.PrintFiscalBill();
