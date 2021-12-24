90fd816d36034758c726ea5fb0b9b63151f4e1b1
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 14:12:25 +0100

    Set3 - Open Closed

--- Set3/SOLID3/Program.cs
+++ Set3/SOLID3/Program.cs
@@ -2,3 +2,13 @@
 
 var instagramPost = new InstagramPost { Photo = new byte[] { } };
 instagramPost.Publish();
+
+
+
+// OpenClosed
+
+
+var calendar = new Calendar();
+calendar.SetEntryTitle("Company updates meeting");
+calendar.InsertEntry("event");
+calendar.GetEntryDate("Company updates meeting");
