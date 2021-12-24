b3088bc90e853eaf56a5d54fc7aa51c98105e3b7
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 12:53:08 +0100

    Set2 - Open Closed

--- Set2/SOLID2/GlobalUsings.cs
+++ Set2/SOLID2/GlobalUsings.cs
@@ -2,7 +2,7 @@
 global using System.Collections.Generic;
 global using System.Globalization;
 global using SOLID2.SingleResponsibility;
-//global using SOLID2.OpenClosed;
+global using SOLID2.OpenClosed;
 //global using SOLID2.LiskovSubstitution;
 //global using SOLID2.InterfaceSegragation;
 //global using SOLID2.DependencyInversion;
