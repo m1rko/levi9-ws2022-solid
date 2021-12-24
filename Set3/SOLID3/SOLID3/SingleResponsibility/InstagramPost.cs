d87a0fd2df98d35e98a83a43cd1aa3139a2dfadb
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 10:54:47 +0100

    Set3 - Single Responsibility added

---
+++ Set3/SOLID3/SOLID3/SingleResponsibility/InstagramPost.cs
@@ -0,0 +1,19 @@
+﻿namespace SOLID3.SingleResponsibility
+{
+    internal class InstagramPost
+    {
+        private string _authorsName = "John Doe";
+        public byte[] Photo { get; set; }
+
+
+        public void Publish()
+        {
+
+        }
+
+        public string GetAuthorsName()
+        {
+            return _authorsName;
+        }
+    }
+}
