a18db427271ecb4698f53d8f522244cfb7e4ef9f
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 17:03:36 +0100

    Set3 - Interface Segregation

---
+++ Set3/SOLID3/InterfaceSegregation/Picture.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID3.InterfaceSegregation
+{
+    internal class Picture : ITextObject
+    {
+        public void Fill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+        }
+
+        public void Rotate()
+        {
+        }
+    }
+}
---
+++ Set3/SOLID3/InterfaceSegregation/Text.cs
@@ -0,0 +1,19 @@
+﻿namespace SOLID3.InterfaceSegregation
+{
+    internal class Text : ITextObject
+    {
+        public void Fill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+        }
+
+        public void Rotate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
