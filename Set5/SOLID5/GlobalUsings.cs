1ea405a05a10cdce5d712667a8b73d708a1586f5
Mirko Mijajlović <mirko993@gmail.com>
2021-12-24 14:52:54 +0100

    Set5 - Open Closed

--- Set5/SOLID5/GlobalUsings.cs
+++ Set5/SOLID5/GlobalUsings.cs
@@ -2,7 +2,7 @@
 global using System.Collections.Generic;
 global using System.Globalization;
 global using SOLID5.SingleResponsibility;
-//global using SOLID5.OpenClosed;
+global using SOLID5.OpenClosed;
 //global using SOLID5.LiskovSubstitution;
 //global using SOLID5.InterfaceSegragation;
 //global using SOLID5.DependencyInversion;
--- Set5/SOLID5/Program.cs
+++ Set5/SOLID5/Program.cs
@@ -2,3 +2,13 @@
 
 var radio = new Radio { Name = "Radio Flash FM"};
 radio.Broadcast(101.00m);
+
+
+
+// OpenClosed
+
+var photoEditor = new PhotoEditor();
+photoEditor.OpenPhoto(new byte[] { });
+photoEditor.SetFilter("bw");
+photoEditor.SetFilterIntensity(1);
+photoEditor.GetEditedPhoto();
