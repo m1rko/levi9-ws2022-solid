bebfad5aedb3de0dc25fbc545ff2caa971fa8a72
Mirko Mijajlović <mirko993@gmail.com>
2021-12-26 22:03:36 +0100

    Set5 - Dependency Inversion

---
+++ Set5/SOLID5/DependencyInversion/LAN.cs
@@ -0,0 +1,22 @@
+﻿namespace SOLID5.DependencyInversion
+{
+    internal class LAN
+    {
+        private NetworkFileTransfer _networkFileTransfer = new NetworkFileTransfer();
+
+        public void OpenConnection()
+        {
+
+        }
+
+        public void TransferFile(string file)
+        {
+            _networkFileTransfer.File = file;
+        }
+
+        public void CloseConnection()
+        {
+
+        }
+    }
+}
---
+++ Set5/SOLID5/DependencyInversion/NetworkFileTransfer.cs
@@ -0,0 +1,22 @@
+﻿namespace SOLID5.DependencyInversion
+{
+    internal class NetworkFileTransfer
+    {
+        public string File { get; set; }
+
+        public void LoadFile()
+        {
+
+        }
+
+        public void Send()
+        {
+
+        }
+
+        public void Receive()
+        {
+
+        }
+    }
+}
