dd6bcc869d036d4f554b5d6717e9a78509768d63
Mirko Mijajlović <mirko993@gmail.com>
2021-12-26 21:38:54 +0100

    Set2 - Dependency Inversion

---
+++ Set2/SOLID2/DependencyInversion/BtFileTransfer.cs
@@ -0,0 +1,17 @@
+﻿namespace SOLID2.DependencyInversion
+{
+    internal class BtFileTransfer
+    {
+        public string File { get; set; }
+
+        public void LoadFile(string path)
+        {
+
+        }
+
+        public void Send()
+        {
+
+        }
+    }
+}
