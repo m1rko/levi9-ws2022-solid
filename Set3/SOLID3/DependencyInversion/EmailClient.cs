d86866739953555d10dcab4b61f8872090d00c8d
Mirko Mijajlović <mirko993@gmail.com>
2021-12-26 21:44:50 +0100

    Set3 - Dependency Inversion

---
+++ Set3/SOLID3/DependencyInversion/EmailClient.cs
@@ -0,0 +1,17 @@
+﻿namespace SOLID3.DependencyInversion
+{
+    internal class EmailClient
+    {
+        public string Email { get; set; }
+
+        public void Open()
+        {
+
+        }
+
+        public void Send()
+        {
+
+        }
+    }
+}
--- Set3/SOLID3/GlobalUsings.cs
+++ Set3/SOLID3/GlobalUsings.cs
@@ -4,4 +4,4 @@ global using SOLID3.SingleResponsibility;
 global using SOLID3.OpenClosed;
 global using SOLID3.LiskovSubstitution;
 global using SOLID3.InterfaceSegregation;
-//global using SOLID3.DependencyInversion;
+global using SOLID3.DependencyInversion;
